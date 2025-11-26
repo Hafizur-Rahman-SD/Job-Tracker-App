f31ba4313a5d5294a622ab6573cf248c44e6f42d
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-11-26 13:20:58 +0600

    c

--- web/src/pages/public/About.jsx
+++ web/src/pages/public/About.jsx
@@ -89,6 +89,17 @@ export default function About() {
         </div>
       </section>
 */}
+
+ 
+  return (
+    <div className="max-w-3xl mx-auto p-6 space-y-3">
+      <h1 className="text-3xl font-bold text-gray-800">Contact:01605135004</h1>
+      <p className="text-gray-600">Email: hafizurrahman5004@gmail.com</p>
+      <p className="text-gray-600">Email: support@jobtracker.com</p>
+    </div>
+  );
+
+
       {/* Footer Section */}
       <footer className="about-footer" data-aos="fade-up">
         <p>© {new Date().getFullYear()} JobTracker | Made with ❤️ Hafizur Rahman</p>
--- web/src/pages/public/Contact.jsx
+++
@@ -1,9 +0,0 @@
-export default function Contact() {
-  return (
-    <div className="max-w-3xl mx-auto p-6 space-y-3">
-      <h1 className="text-3xl font-bold text-gray-800">Contact:01605135004</h1>
-      <p className="text-gray-600">Email: hafizurrahman5004@gmail.com</p>
-      <p className="text-gray-600">Email: support@jobtracker.com</p>
-    </div>
-  );
-}
